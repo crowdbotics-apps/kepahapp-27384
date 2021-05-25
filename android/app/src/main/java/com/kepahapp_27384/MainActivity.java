7841fa18585cbfb89ba20d4359a6699b62990250
Crowdbotics <team@crowdbotics.com>
2021-05-25 17:12:43 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/kepahapp_27384/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kepahapp_27384;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "kepahapp_27384";
+  }
+}
